3e78f4b19228cc629a6827a5c42992fc3c292c6f
maryane-castro <maryane.castro993@gmail.com>
2023-03-07 16:55:33 -0300

    .

--- geodata/geodata/settings.py
+++ geodata/geodata/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6&c5fe)^lugysrck3n+7_s9&c%i1^jr5ntb5n2xrqyay=$xav#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
